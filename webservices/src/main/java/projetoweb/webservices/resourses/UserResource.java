d5b631c40713459b9bd9566b64d5593a8fc44c2e
v-andrade-dev <vini.andrade0@gmail.com>
2022-12-12 16:11:18 -0300

    camada de serviço e registro de componentes

--- webservices/src/main/java/projetoweb/webservices/resourses/UserResource.java
+++ webservices/src/main/java/projetoweb/webservices/resourses/UserResource.java
@@ -1,24 +1,36 @@
 package projetoweb.webservices.resourses;
 
+import java.util.List;
+
+import org.apache.catalina.connector.Response;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import projetoweb.webservices.entities.User;
+import projetoweb.webservices.service.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-
-	public UserResource() {
-		
-	}
+	
+	@Autowired
+	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "1155889933", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+			
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
